785fec5e5c10818805671db10a7063dd96e7881a
elizabethgutierrez27 <146129308+elizabethgutierrez27@users.noreply.github.com>
2025-04-08 13:13:40 -0600

    ok

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,24 +3,20 @@ import { InicioComponent } from './components/inicio/inicio.component';
 import { CrearClienteComponent } from './components/crear-cliente/crear-cliente.component';
 import { ListarClientesComponent } from './components/listar-cliente/listar-cliente.component';
 import { ActualizarClienteComponent } from './components/actualizar-cliente/actualizar-cliente.component';
-<<<<<<<<< Temporary merge branch 1
 import { RegisterComponent } from './components/register/register.component';
 import { LoginComponent } from './components/login/login.component';
 import { RecuperarContrasenaComponent } from './components/recuperar-contrasena/recuperar-contrasena.component';
 import { OtpVerificationComponent } from './components/otp-verification/otp-verification.component';
 import { SeleccionarCanalComponent } from './components/seleccionar-canal/seleccionar-canal.component';
-=========
 import { NavbarComponent } from './components/navbar/navbar.component';
->>>>>>>>> Temporary merge branch 2
 
 export const routes: Routes = [
   { path: '', component: InicioComponent },
   { path: 'crearCliente', component: CrearClienteComponent },
   {path: 'listarClientes', component:ListarClientesComponent},
   { path: 'actualizarCliente', component: ActualizarClienteComponent },
-<<<<<<<<< Temporary merge branch 1
   { path:'registro', component:RegisterComponent},
-  {path:'navbar', component: NavbarComponent}
+  {path:'navbar', component: NavbarComponent},
   { path:'login', component:LoginComponent},
   { path:'recuperar-contrasena', component:RecuperarContrasenaComponent},
   { path:'verificar-otp', component:OtpVerificationComponent},
